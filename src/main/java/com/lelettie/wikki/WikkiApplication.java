6fb36b2e877ccdce29a52b910d24978b197c9536
LeleSc <143450715+LeleSc@users.noreply.github.com>
2024-05-23 13:35:48 -0400

    v0.0 init

---
+++ src/main/java/com/lelettie/wikki/WikkiApplication.java
@@ -0,0 +1,13 @@
+package com.lelettie.wikki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikkiApplication.class, args);
+    }
+
+}
