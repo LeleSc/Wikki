da1e41954bb59d7275f6b3701b381a2707955393
LeleSc <143450715+LeleSc@users.noreply.github.com>
2024-05-23 19:35:56 -0400

    v0.2 revise initialize log

--- src/main/java/com/lelettie/wikki/WikkiApplication.java
+++ src/main/java/com/lelettie/wikki/WikkiApplication.java
@@ -1,13 +1,19 @@
 package com.lelettie.wikki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikkiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikkiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikkiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikkiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Initialization Successful!");
+        LOG.info("Address: http://127.0.0.1:{}", env.getProperty("server.port"));
     }
-
 }
