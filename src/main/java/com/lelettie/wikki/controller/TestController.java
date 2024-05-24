cb0a8d25c3efa8e2474540d79b8e5530cd60da07
LeleSc <143450715+LeleSc@users.noreply.github.com>
2024-05-23 22:34:28 -0400

    v0.3 Hello world API, with address: http://127.0.0.1:8880/hello

--- src/main/java/com/lelettie/wikki/controller/TestController.java
+++ src/main/java/com/lelettie/wikki/controller/TestController.java
@@ -1,12 +1,11 @@
 package com.lelettie.wikki.controller;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * RestController would return a string.
- *
  * For a project with a frontend-backend separation, we return a string, so we use @RestController.
  * Alternatively, we can use @Controller to return a page.
  */
