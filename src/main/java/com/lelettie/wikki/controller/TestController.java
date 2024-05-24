7c77d06c9a5a49f652a83bbf766991d14e730b82
LeleSc <143450715+LeleSc@users.noreply.github.com>
2024-05-23 22:57:13 -0400

    v0.4 HTTP Client test

--- src/main/java/com/lelettie/wikki/controller/TestController.java
+++ src/main/java/com/lelettie/wikki/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.lelettie.wikki.controller;
 
 //import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,8 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post, " + name;
     }
 }
